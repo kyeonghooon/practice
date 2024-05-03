fe54eafb421c770e58aa1f022e4db32f0117a6a2
kyeonghooon <gibbon0510@gmail.com>
2024-05-03 15:43:43 +0900

    테스트건드림

--- src/practice/Test.java
+++ src/practice/Test.java
@@ -1,13 +1,11 @@
 package practice;
 
-import java.util.ArrayList;
 
 public class Test {
-	static ArrayList<String> list = new ArrayList<String>();
 
 	public static void main(String[] argv) {
-		list.add("Kim");
-		list.add("Park");
+		int i = new Thread().getPriority();
+		System.out.println(i);
 	}
 
 }
