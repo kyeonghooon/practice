fab21d8dbd8a4643c39b2a5f0d5114db773d0cbb
kyeonghooon <gibbon0510@gmail.com>
2024-05-08 20:19:39 +0900

    수정수정

--- src/practice/projectPractice/BeatFrame.java
+++ src/practice/projectPractice/BeatFrame.java
@@ -13,36 +13,36 @@ public class BeatFrame extends JFrame {
 		initData();
 		setInitLayout();
 		
-		new Thread(() -> {
-			try {
-				Thread.sleep(168000);
-				System.out.println("작동작동");
-				flag = false;
-			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-		}).start();
-		
-		while (flag) {
-			try {
-				Thread.sleep(428, 5714);
-			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-			add(new Bubble());
-		}
+//		new Thread(() -> {
+//			try {
+//				Thread.sleep(168000);
+//				System.out.println("작동작동");
+//				flag = false;
+//			} catch (InterruptedException e) {
+//				// TODO Auto-generated catch block
+//				e.printStackTrace();
+//			}
+//		}).start();
+//		
+//		while (flag) {
+//			try {
+//				Thread.sleep(428, 5714);
+//			} catch (InterruptedException e) {
+//				// TODO Auto-generated catch block
+//				e.printStackTrace();
+//			}
+//			add(new Bubble());
+//		}
 		
 	}
 
 	private void initData() {
 		backgroundMap = new JLabel(new ImageIcon("images/background1.jpg"));
 		setContentPane(backgroundMap);
-		setSize(1000, 600);
+		setSize(1600, 900);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		BGM bgm = new BGM();
-		bgm.gainControl.setValue(-30.0f);
+		// BGM bgm = new BGM();
+		// bgm.gainControl.setValue(-30.0f);
 	}
 
 	private void setInitLayout() {
